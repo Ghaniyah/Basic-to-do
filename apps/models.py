0b9152b4ff7811bb147136276f9ebe2ed136c710
Ghaniyah <gabdulazeez2@gmail.com>
2022-04-24 22:07:21 +0100

    initial commit

---
+++ apps/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+# import user model
+# first option for importing user model
+from django.contrib.auth.models import User
+
+#second option for importing user model
+
+from django.contrib.auth import get_user_model
+
+task_owner = get_user_model()
+
+
+#create your models here.
+
+class TodoData(models.Model):
+    name_of_task = models.CharField(max_length=100) 
+    task  =  models.CharField(max_length=500)
+    date_started = models.DateTimeField(auto_now_add=True)
+    done = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User,on_delete= models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.name_of_task
+    
+    # plural of the model name
+    class Meta:
+        verbose_name_plural = 'Todo Data'
---
+++ apps/views.py
@@ -0,0 +1,58 @@
+from asyncio import all_tasks
+from django.shortcuts import render, redirect
+from django.views import generic
+from .models import TodoData
+from django.http import HttpResponseRedirect
+from .forms import Todo_Task_form
+
+# Create your views here.
+#CRUD CREATE,READ,UPDATE,DELETE
+
+
+# read
+
+def  ListTask(request):
+    context= {}
+    all_tasks= TodoData.objects.all()
+    context['all_tasks']=all_tasks
+    return render(request, 'homepage.html',context)
+
+#create
+def CreateTask(request):
+    if request.method=='POST':
+        forms=Todo_Task_form(request.POST)
+        if forms.is_valid():
+            new_todo = forms.save(commit=False)
+            new_todo.save()
+            return redirect("listtask")
+        else:
+            forms = Todo_Task_form()
+    else:
+        forms = Todo_Task_form()
+         
+    return render(request,'create.html',{'forms':forms})
+
+#delete
+def DeleteTask(request,pk):
+    tod_task = TodoData.objects.all().filter(pk=pk)
+    tod_task.delete()
+    return redirect('/')
+
+
+# update
+def UpdateTask(request,pk):
+    todoapp_in_question = TodoData.objects.get(pk=pk)
+    formUpdate = Todo_Task_form(request.POST, instance=todoapp_in_question)
+    
+    
+    if request.method== "POST":
+        if formUpdate.is_valid():
+            formUpdate.save()
+            return redirect("/")
+        else:
+            return render(request,"update.html",{'formUpdate':formUpdate})
+    return render(request,"update.html",{'formUpdate':formUpdate})    
+    
+            
+    
+
